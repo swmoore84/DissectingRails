e9fc236270c5c069d3c434225f28dcf4c37b855f
sam moore <swmoore84@gmail.com>
2021-03-22 15:25:23 +0900

    create topic table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_045339) do
+ActiveRecord::Schema.define(version: 2021_03_22_062217) do
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
